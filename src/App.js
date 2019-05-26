ad8e1245bb7ac01d78bcfcbb31f5c20eb637c370
agnxr <32043294+agnxr@users.noreply.github.com>
2019-05-26 21:09:22 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,20 +1,109 @@
 import React from 'react';
-import ListWrapper from './components/ListWrapper/ListWrapper';
-import './index.css';
-import SearchBar from '../src/components/SearchBar/SearchBar';
+import './App.css';
+import reports from './data/reports';
+// z braku laku istniających userów definiuję po prostu jako stałą
+// w prawdziwej aplikacji pochodziliby z API i/lub byli w Reduksowym storze 
+//const allUsers = ['Michal', 'Kasia', 'Jacek', 'Marta', 'Tomek', 'Ania'];
+let users = []; //{...}, {...}, {...}
+
+reports.map((item) => (
+    users.push(item)
+));
+
+console.log(users);
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-            <h1>Rapoty</h1>
-            <div>
-            <SearchBar />
-            <ListWrapper />
-        </div>
-        </div>
-        );
-    }
+  constructor() {
+    super();    
+    
+    this.state = {
+      //filteredUsers: allUsers
+      filteredUsers: users,
+
+    };
+  }
+
+  filterUsers(e) {
+    const text = e.currentTarget.value; //wpisana wartosc
+    const filteredUsers = this.getFilteredUsersForText(text)
+    this.setState({
+      filteredUsers
+    })
+  }
+  
+  getFilteredUsersForText(text) {
+    
+    //po tytule lub opisie
+ return reports.filter(x => x.title.toLowerCase().includes(text.toLowerCase()) || x.description.toLowerCase().includes(text.toLowerCase()))
+//po opisie
+//return reports.filter(x => x.description.toLowerCase().includes(text.toLowerCase()))
+   // return reports.filter(x => x.title.toLowerCase().includes(text.toLowerCase())) 
+
+  // return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()))
+  }
+  
+  render () {
+    return (
+      <div>
+        
+        <input onInput={this.filterUsers.bind(this)} />
+        <UsersList users={this.state.filteredUsers} />
+        
+      </div>
+    );
+  }
+};
+
+class ListItem extends React.Component {
+
+  render() {
+return (
+  <li className="listItem__wrapper">
+      <div>
+          <p>
+              {this.props.date}
+          </p>
+          <p>
+              {this.props.hour}
+          </p>
+          <p>
+              {this.props.category}
+          </p>
+      </div>
+      <div>
+          <h2 className="listItem__name">
+          {this.props.title}
+          </h2>
+          <p className="listItem__description">
+          {this.props.description}
+          </p>
+          {  this.props.links}
+      </div>
+
+
+  </li>
+)
+  }
 }
+
+const UsersList = ({ users }) => {
+  if (users.length > 0) {
+    return (
+      <ul>
+        {users.map(item => 
+            <ListItem key={item}
+            category={item.category}
+            title={item.title}
+            description={item.description}/>
+        )}
+        
+      </ul>
+    );
+  }
+
+  return (
+    <p>No results!</p>
+  );
+};
 
 export default App;
---
+++ src/data/reports.js
@@ -0,0 +1,39 @@
+const reports = [
+    {
+      "date": 1407087996097,
+      "category": "Finansowe",
+      "title": "Dolor ut ut Lorem exercitation irure non voluptate ea laboris proident ipsum ut velit enim.",
+      "description": "Magna velit non proident minim ad incididunt.",
+      "files": [
+        {
+          "filename": "dolore",
+          "filesize": 240
+        }
+      ]
+    },
+    {
+      "date": 1321670688864,
+      "category": "Roczne",
+      "title": "Tempor officia ad culpa eiusmod qui anim dolor velit elit irure.",
+      "description": "Non occaecat excepteur nostrud Lorem ex dolor ad.",
+      "files": []
+    },
+    {
+      "date": 1277370542975,
+      "category": "Finansowe",
+      "title": "Incididunt pariatur ea aute aliqua nulla occaecat laboris.",
+      "description": "Occaecat nostrud anim esse consequat.",
+      "files": [
+        {
+          "filename": "adipisicing",
+          "filesize": 779
+        },
+        {
+          "filename": "velit",
+          "filesize": 604
+        }
+      ]
+    }
+  ]
+ 
+  export default reports;
